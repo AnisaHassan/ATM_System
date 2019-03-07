ef3b1405bd31fd100273b6d8a83c36dcfe00963d
Kajene <au594051@post.au.dk>
2019-03-07 11:42:07 +0100

    Added attributes

--- ATM_System/TrackInfo.cs
+++ ATM_System/TrackInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,21 @@ namespace ATM_System
 {
     class TrackInfo
     {
+        private string _tag { get; set; }
+        private int _xcoor { get; set; }
+        private int _ycoor { get; set; }
+        private int _altitude { get; set; }
+        private DateTime _date; 
+
+        public TrackInfo (string tag, int xcoor, int ycoor, int altitude, DateTime date)
+        {
+            _tag = tag;
+            _xcoor = xcoor;
+            _ycoor = ycoor;
+            _altitude = altitude;
+            _date = date; 
+        }
+
     }
+
 }
