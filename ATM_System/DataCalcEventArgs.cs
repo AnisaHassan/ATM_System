241fd61d1bfbaaa78f9de46f5e1293b0b7d2add4
Anisa Hassan <au592172@post.au.dk>
2019-03-13 11:43:12 +0100

    Added ny event

---
+++ ATM_System/DataCalcEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM_System
+{
+    public class DataCalcEventArgs
+    {
+        public List<Plane> DataList { get; }
+        public DataCalcEventArgs(List<Plane> trackedInfo)
+        {
+            this.DataList = trackedInfo;
+        }
+    }
+}
--- ATM_System/Interfaces/IDataCalculator.cs
+++ ATM_System/Interfaces/IDataCalculator.cs
@@ -8,5 +8,6 @@ namespace ATM_System
 {
    public interface IDataCalculator
     {
+        
     }
 }
