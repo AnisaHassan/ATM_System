e423a961a7428f6f467fa8951471788d3d9afe65
kbjerringfelt <karen.bjerringfelt@gmail.com>
2019-03-17 12:33:52 +0100

    ændret lidt på calculator

--- ATM_System/DataCalculator.cs
+++ ATM_System/DataCalculator.cs
@@ -11,7 +11,6 @@ namespace ATM_System
     {
         private ITrackInfo _dataCalcRecieved;
         public List<Plane> nyliste { get; set; }
-        public List<Plane> gammelliste { get; set; }
 
         public DataCalculator(ITrackInfo dataCalcRecieved)
         {
@@ -27,7 +26,11 @@ namespace ATM_System
             
             foreach (var plane in list)
             {
-                nyliste = list;
+                nyliste.Add(plane);
+
+                CalculateVelocity(list, nyliste);
+
+                CalculateCourse(list, nyliste);
                 Console.WriteLine("Tag: " + plane._tag + "\nX-coordinate: " + plane._xcoor + " meters\nY-coordinate: " +
                                   plane._ycoor + " meters\nAltitude: " + plane._altitude + " meters\nTime stamp: " +
                                   plane._time.Year + "/" + plane._time.Month + "/" + plane._time.Day +
@@ -37,62 +40,53 @@ namespace ATM_System
                                   plane._compassCourse + " degrees\n");
             }
 
+
         }
-        //public void CalculateVelocity(Plane planeOld, Plane planeNew)
-        //{
-
-        //    double distance = Math.Sqrt(Math.Pow(planeOld._xcoor - planeNew._xcoor, 2) +
-        //                                Math.Pow(planeOld._ycoor - planeNew._ycoor, 2) +
-        //                                Math.Pow(planeOld._altitude - planeNew._altitude, 2));
-        //    double time = (planeOld._time - planeNew._time).TotalSeconds;
-
-        //    planeNew._velocity = (distance / time);
 
 
-        //}
+        public void CalculateVelocity(List<Plane> planeOld, List<Plane> planeNew)
+        {
+            foreach (var planeO in planeOld)
+            {
+                foreach (var planeN in planeNew)
+                {
+                    double distance = Math.Sqrt(Math.Pow(planeO._xcoor - planeN._xcoor, 2) +
+                                                Math.Pow(planeO._ycoor - planeN._ycoor, 2) +
+                                                Math.Pow(planeO._altitude - planeN._altitude, 2));
+                    double time = (planeO._time - planeN._time).TotalSeconds;
 
-        //public void CalculateCourse(Plane planeOld, Plane planeNew)
-        //{
-        //    double slope = (planeOld._ycoor - planeNew._ycoor) / (planeOld._xcoor - planeNew._xcoor);
-
-        //    planeNew._compassCourse = 90 - ((Math.Atan(slope) * 1800) / Math.PI);
-
-        //}
-
-        //public void CalculateVelocity(List<Plane> planeOld, List<Plane> planeNew)
-        //{
-        //    foreach (var planeO in planeOld)
-        //    {
-        //        foreach (var planeN in planeNew)
-        //        {
-        //            double distance = Math.Sqrt(Math.Pow(planeO._xcoor - planeN._xcoor, 2) +
-        //                                        Math.Pow(planeO._ycoor - planeN._ycoor, 2) +
-        //                                        Math.Pow(planeO._altitude - planeN._altitude, 2));
-        //            double time = (planeO._time - planeN._time).TotalSeconds;
-
-        //            planeN._velocity = (distance / time);
-        //        }
-        //    }
+                    planeN._velocity = (distance / time);
+                }
+            }
 
 
-        //}
+        }
 
-        //public void CalculateCourse(List<Plane> planeOld, List<Plane> planeNew)
-        //{
-        //    foreach (var planeO in planeOld)
-        //    {
-        //        foreach (var planeN in planeNew)
-        //        {
-        //            if (planeN._tag == planeO._tag)
-        //            {
-        //                double slope = (planeO._ycoor - planeN._ycoor) / (planeO._xcoor - planeN._xcoor);
+        public void CalculateCourse(List<Plane> planeOld, List<Plane> planeNew)
+        {
+            foreach (var planeO in planeOld)
+            {
+                foreach (var planeN in planeNew)
+                {
+                    if (planeN._tag == planeO._tag)
+                    {
+                        if (planeO._xcoor-planeN._xcoor !=0)
+                        {
+                            double slope = (planeO._ycoor - planeN._ycoor) / (planeO._xcoor - planeN._xcoor);
 
-        //                planeN._compassCourse = 90 - ((Math.Atan(slope) * 1800) / Math.PI);
-        //            }
+                            planeN._compassCourse = 90 - ((Math.Atan(slope) * 180) / Math.PI);
+                        }
 
-        //        }
-        //    }
+                        else
+                        {
+                            planeN._compassCourse = 0;
+                        }
+                        
+                    }
 
-        //}
+                }
+            }
+
+        }
     }
 }
