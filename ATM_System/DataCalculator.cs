125fceea8fe95082bf76491b01a002538225730e
Anisa Hassan <au592172@post.au.dk>
2019-03-19 12:26:02 +0100

    Added gammellist. Klar til test

--- ATM_System/DataCalculator.cs
+++ ATM_System/DataCalculator.cs
@@ -13,44 +13,40 @@ namespace ATM_System
         private ITrackInfo _dataCalcRecieved;
         public List<Plane> nyliste { get; set; }
         public List<Plane> gammelliste { get; set; }
-
         public DataCalculator(ITrackInfo dataCalcRecieved)
         {
             this._dataCalcRecieved = dataCalcRecieved;
 
             this._dataCalcRecieved.AirspaceDataReady += UseList;
-            nyliste = new List<Plane>();
-             gammelliste = new List<Plane>();
+            
         }
 
-        public DataCalculator()
-        {
-           
-        }
-
+        public DataCalculator() { }
+        
         public void UseList(object sender, DataCalcEventArgs e)
         {
-            gammelliste = nyliste;
-            nyliste = e.DataList;
+            var list = e.DataList;
+            nyliste.Clear();
             
-            foreach (var plane in nyliste)
+            foreach (var plane in list)
             {
-                //nyliste.Add(plane);
+                nyliste.Add(plane);
+                
 
-                CalculateVelocity(gammelliste, nyliste);
-
-                CalculateCourse(gammelliste, nyliste);
-
-                Console.WriteLine("Tag: " + plane._tag + "\nX-coordinate: " + plane._xcoor + " meters\nY-coordinate: " +
-                                  plane._ycoor + " meters\nAltitude: " + plane._altitude + " meters\nTime stamp: " +
-                                  plane._time.Year + "/" + plane._time.Month + "/" + plane._time.Day +
-                                  ", at " + plane._time.Hour + ":" + plane._time.Minute + ":" +
-                                  plane._time.Second + " and " + plane._time.Millisecond +
-                                  " milliseconds \nVelocity: " + plane._velocity + " m/s\nCourse: " +
-                                  plane._compassCourse + " degrees\n");
+                //Console.WriteLine("Tag: " + plane._tag + "\nX-coordinate: " + plane._xcoor + " meters\nY-coordinate: " +
+                //                  plane._ycoor + " meters\nAltitude: " + plane._altitude + " meters\nTime stamp: " +
+                //                  plane._time.Year + "/" + plane._time.Month + "/" + plane._time.Day +
+                //                  ", at " + plane._time.Hour + ":" + plane._time.Minute + ":" +
+                //                  plane._time.Second + " and " + plane._time.Millisecond +
+                //                  " milliseconds \nVelocity: " + plane._velocity + " m/s\nCourse: " +
+                //                  plane._compassCourse + " degrees\n");
             }
 
+            CalculateVelocity(gammelliste, nyliste);
 
+            CalculateCourse(gammelliste, nyliste);
+
+            gammelliste = new List<Plane>(nyliste);
         }
 
 
@@ -60,12 +56,16 @@ namespace ATM_System
             {
                 foreach (var planeN in planeNew)
                 {
-                    double distance = Math.Sqrt(Math.Pow(planeO._xcoor - planeN._xcoor, 2) +
-                                                Math.Pow(planeO._ycoor - planeN._ycoor, 2) +
-                                                Math.Pow(planeO._altitude - planeN._altitude, 2));
-                    double time = (planeO._time - planeN._time).TotalSeconds;
+                    if (planeN._tag == planeO._tag)
 
-                    planeN._velocity = (distance / time);
+                    {
+                        double distance = Math.Sqrt(Math.Pow(planeO._xcoor - planeN._xcoor, 2) +
+                                                    Math.Pow(planeO._ycoor - planeN._ycoor, 2) +
+                                                    Math.Pow(planeO._altitude - planeN._altitude, 2));
+                        double time = (planeO._time - planeN._time).TotalSeconds;
+
+                        planeN._velocity = (distance / time);
+                    }
                 }
             }
 
