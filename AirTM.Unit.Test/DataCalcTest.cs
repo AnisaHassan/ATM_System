5cdb4703cf8fe6d1d768d190e518f8c740937ef1
Kajene <au594051@post.au.dk>
2019-03-19 13:57:38 +0100

    MERGE PROBLEM

--- AirTM.Unit.Test/DataCalcTest.cs
+++ AirTM.Unit.Test/DataCalcTest.cs
@@ -25,7 +25,7 @@ namespace AirTM.Unit.Test
           uut = new DataCalculator();
           var dateTime1 = new DateTime(2018, 04, 05, 20, 20, 18);
           var dateTime2 = new DateTime(2018, 04, 05, 20, 20, 20);
-            _track1 = new Plane
+            _plane1 = new Plane
           {
               _tag = "ART123",
               _xcoor = 12345,
@@ -49,8 +49,8 @@ namespace AirTM.Unit.Test
           };
           planelist = new List<Plane>
           {
-              _track1,
-              _track2
+              _plane1,
+              _plane1
           };
 
         }
@@ -62,6 +62,12 @@ namespace AirTM.Unit.Test
           Assert.That(Math.Round(planelist[0]._velocity, 2), Is.EqualTo(44614.55));
       }
 
+      [Test]
+        public void coursecompass_isCorrect()
+      {
+          uut.CalculateCourse(gammelliste, planelist);
+          Assert.That(Math.Round(planelist[0]._compassCourse), Is.EqualTo(44614.55));
+        }
 
 
 }
