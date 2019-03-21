2ffa2133b17bd3176b1d0ecc5d8f138450c8f206
Anisa Hassan <au592172@post.au.dk>
2019-03-21 21:41:51 +0100

    added test

--- AirTM.Unit.Test/DataCalcTest.cs
+++ AirTM.Unit.Test/DataCalcTest.cs
@@ -20,6 +20,8 @@ namespace AirTM.Unit.Test
         private List<Plane> gammelliste;
         private Plane _plane1;
         private Plane _plane2;
+        private Plane _plane3;
+        private Plane _plane4;
 
         [SetUp]
         public void SetUp()
@@ -28,6 +30,8 @@ namespace AirTM.Unit.Test
             uut = new DataCalculator(new TrackInfo());
             var dateTime1 = new DateTime(2019, 06, 05, 10, 54, 34);
             var dateTime2 = new DateTime(2019, 06, 05, 10, 54, 50);
+            var dateTime3 = new DateTime(2019, 06, 05, 10, 50, 00);
+            var dateTime4 = new DateTime(2019, 06, 05, 10, 50, 10);
             _plane1 = new Plane
             {
                 _tag = "ART123",
@@ -48,13 +52,30 @@ namespace AirTM.Unit.Test
                 _time = dateTime2,
             };
 
+            _plane3 = new Plane
+            {
+                _tag = "TRE123",
+                _xcoor = 6,
+                _ycoor = 4,
+                _altitude = 12345,
+                _time = dateTime3,
+
+
+            };
+
+            _plane4 = new Plane
+            {
+                _tag = "TRE123",
+                _xcoor = 9,
+                _ycoor = 8,
+                _altitude = 12345,
+                _time = dateTime4,
+            };
         }
 
         [Test]
         public void velocity_isCorrect()
         {
-
-
             planelist = new List<Plane>();
             planelist.Add(_plane1);
             gammelliste = new List<Plane>();
@@ -67,7 +88,21 @@ namespace AirTM.Unit.Test
             Assert.That(uut.nyliste[0]._velocity, Is.EqualTo(5659.97));
 
         }
+        [Test]
+        public void velocity_med_3_4_Correct()
+        {
+            planelist = new List<Plane>();
+            planelist.Add(_plane3);
+            gammelliste = new List<Plane>();
+            gammelliste.Add(_plane4);
 
+            uut.gammelliste = gammelliste;
+            uut.nyliste = planelist;
+            uut.CalculateVelocity();
+
+            Assert.That(uut.nyliste[0]._velocity, Is.EqualTo(5));
+
+        }
         [Test]
         public void compass_isCorrect()
         {
