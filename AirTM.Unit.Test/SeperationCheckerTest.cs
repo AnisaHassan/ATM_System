67ddf6b9bc3895035183422fb5941b3ca12f9aaa
kbjerringfelt <karen.bjerringfelt@gmail.com>
2019-03-22 11:42:58 +0100

    sep

--- AirTM.Unit.Test/SeperationCheckerTest.cs
+++ AirTM.Unit.Test/SeperationCheckerTest.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace AirTM.Unit.Test
 {
-    class SeperationCheckerTest
+    [TestFixture]
+    public class SeperationCheckerTest
     {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
     }
 }
