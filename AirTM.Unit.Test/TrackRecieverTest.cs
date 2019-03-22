9881b802a43f7c83589708b8de617d8eb072d374
Anisa Hassan <au592172@post.au.dk>
2019-03-22 11:10:30 +0100

    Brugt NSubstitude i TrackReciverTest

--- AirTM.Unit.Test/TrackRecieverTest.cs
+++ AirTM.Unit.Test/TrackRecieverTest.cs
@@ -25,26 +25,27 @@ namespace AirTM.Unit.Test
         {
             _fakeTransponderReceiver = Substitute.For<ITransponderReceiver>();
             _uut = new TrackReciever();
-
-        
-            stringList = new List<string>();
-            stringList.Add("ATR423;39045;12932;14000;20151006213456789");
-            stringList.Add("ABC121;12305;87501;13000;20151006213456779");
-            stringList.Add("DEF182;46000;87654;12000;20151006213456769");
-            stringList.Add("XKJ967;90239;99876;11000;20151006213456759");
-
-            planeList = _uut.TrackedInfo(stringList);
+                        
 
         }
 
         [Test]
         public void Test_with_faketransponderreciver()
         {
+            stringList = new List<string>();
+            stringList.Add("ATR423;39045;12932;14000;20151006213456789");
+            stringList.Add("ABC121;12305;87501;13000;20151006213456779");
+            stringList.Add("DEF182;46000;87654;12000;20151006213456769");
+            stringList.Add("XKJ967;90239;99876;11000;20151006213456759");
+
             _fakeTransponderReceiver.TransponderDataReady 
                 += Raise.EventWith(this, new RawTransponderDataEventArgs(stringList));
 
+            //planeList = _uut.TrackedInfo(stringList);
+            //Assert.That(planeList.Count.Equals(4));
 
-            Assert.That(planeList.Count.Equals(4));
+            Assert.That(_uut.TrackedInfo(stringList).Count, Is.EqualTo(4));
+
 
         }
 
@@ -53,28 +54,54 @@ namespace AirTM.Unit.Test
         [Test]
         public void planeList_tag_isCorrect()
         {
-            //planeList[0] = Første linje i listen 
-          
+            stringList = new List<string>();
+            stringList.Add("ATR423;39045;12932;14000;20151006213456789");
+           
+            _fakeTransponderReceiver.TransponderDataReady
+                += Raise.EventWith(this, new RawTransponderDataEventArgs(stringList));
+
+            planeList = _uut.TrackedInfo(stringList);
+
             Assert.That(planeList[0]._tag, Is.EqualTo("ATR423"));
         }
 
         [Test]
         public void planeList_xcoor_isCorrect()
         {
-         
+            stringList = new List<string>();
+            stringList.Add("ATR423;39045;12932;14000;20151006213456789");
+
+            _fakeTransponderReceiver.TransponderDataReady
+                += Raise.EventWith(this, new RawTransponderDataEventArgs(stringList));
+
+            planeList = _uut.TrackedInfo(stringList);
             Assert.That(planeList[0]._xcoor, Is.EqualTo(39045));
         }
         [Test]
         public void planeList_ycoor_isCorrect()
         {
-       
+            stringList = new List<string>();
+            stringList.Add("ATR423;39045;12932;14000;20151006213456789");
+
+            _fakeTransponderReceiver.TransponderDataReady
+                += Raise.EventWith(this, new RawTransponderDataEventArgs(stringList));
+
+            planeList = _uut.TrackedInfo(stringList);
+
             Assert.That(planeList[0]._ycoor, Is.EqualTo(12932));
         }
 
         [Test]
         public void planeList_alitude_isCorrect()
         {
-         
+            stringList = new List<string>();
+            stringList.Add("ATR423;39045;12932;14000;20151006213456789");
+
+            _fakeTransponderReceiver.TransponderDataReady
+                += Raise.EventWith(this, new RawTransponderDataEventArgs(stringList));
+
+            planeList = _uut.TrackedInfo(stringList);
+
             Assert.That(planeList[0]._altitude, Is.EqualTo(14000));
         }
 
@@ -82,22 +109,41 @@ namespace AirTM.Unit.Test
         [Test]
         public void planeList_Timestamp_Year_isCorrect()
         {
-           
 
+            stringList = new List<string>();
+            stringList.Add("ATR423;39045;12932;14000;20151006213456789");
+
+            _fakeTransponderReceiver.TransponderDataReady
+                += Raise.EventWith(this, new RawTransponderDataEventArgs(stringList));
+
+            planeList = _uut.TrackedInfo(stringList);
 
             Assert.That(planeList[0]._time.Year, Is.EqualTo(2015));
         }
         [Test]
         public void planeList_Timestamp_Month_isCorrect()
         {
-       
+            stringList = new List<string>();
+            stringList.Add("ATR423;39045;12932;14000;20151006213456789");
+
+            _fakeTransponderReceiver.TransponderDataReady
+                += Raise.EventWith(this, new RawTransponderDataEventArgs(stringList));
+
+            planeList = _uut.TrackedInfo(stringList);
+
             Assert.That(planeList[0]._time.Month, Is.EqualTo(10));
         }
 
         [Test]
         public void planeList_Timestamp_Day_isCorrect()
         {
-            
+            stringList = new List<string>();
+            stringList.Add("ATR423;39045;12932;14000;20151006213456789");
+
+            _fakeTransponderReceiver.TransponderDataReady
+                += Raise.EventWith(this, new RawTransponderDataEventArgs(stringList));
+
+            planeList = _uut.TrackedInfo(stringList);
 
             Assert.That(planeList[0]._time.Day, Is.EqualTo(06));
         }
@@ -105,8 +151,13 @@ namespace AirTM.Unit.Test
         [Test]
         public void planeList_Timestamp_Hour_isCorrect()
         {
-           
+            stringList = new List<string>();
+            stringList.Add("ATR423;39045;12932;14000;20151006213456789");
 
+            _fakeTransponderReceiver.TransponderDataReady
+                += Raise.EventWith(this, new RawTransponderDataEventArgs(stringList));
+
+            planeList = _uut.TrackedInfo(stringList);
 
             Assert.That(planeList[0]._time.Hour, Is.EqualTo(21));
         }
@@ -114,7 +165,13 @@ namespace AirTM.Unit.Test
         [Test]
         public void planeList_Timestamp_Minutes_isCorrect()
         {
-            
+            stringList = new List<string>();
+            stringList.Add("ATR423;39045;12932;14000;20151006213456789");
+
+            _fakeTransponderReceiver.TransponderDataReady
+                += Raise.EventWith(this, new RawTransponderDataEventArgs(stringList));
+
+            planeList = _uut.TrackedInfo(stringList);
 
 
             Assert.That(planeList[0]._time.Minute, Is.EqualTo(34));
@@ -123,14 +180,27 @@ namespace AirTM.Unit.Test
         [Test]
         public void planeList_Timestamp_Second_isCorrect()
         {
-        
+            stringList = new List<string>();
+            stringList.Add("ATR423;39045;12932;14000;20151006213456789");
+
+            _fakeTransponderReceiver.TransponderDataReady
+                += Raise.EventWith(this, new RawTransponderDataEventArgs(stringList));
+
+            planeList = _uut.TrackedInfo(stringList);
+
             Assert.That(planeList[0]._time.Second, Is.EqualTo(56));
         }
 
         [Test]
         public void planeList_Timestamp_Millisecond_isCorrect()
         {
-      
+            stringList = new List<string>();
+            stringList.Add("ATR423;39045;12932;14000;20151006213456789");
+
+            _fakeTransponderReceiver.TransponderDataReady
+                += Raise.EventWith(this, new RawTransponderDataEventArgs(stringList));
+
+            planeList = _uut.TrackedInfo(stringList);
 
             Assert.That(planeList[0]._time.Millisecond, Is.EqualTo(789));
         }
