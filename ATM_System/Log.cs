d1143d759321f30d3dfeb58b734b0ee15fc23689
kbjerringfelt <karen.bjerringfelt@gmail.com>
2019-03-25 15:52:48 +0100

    pakker

--- ATM_System/Log.cs
+++ ATM_System/Log.cs
@@ -15,7 +15,7 @@ namespace ATM_System
         }
         public void PrintWarning(Plane plane1, Plane plane2)
         {
-            using (StreamWriter fileWriter = new FileInfo("Seperationslog.txt").AppendText())
+            using (StreamWriter fileWriter = new StreamWriter("Seperationslog.txt"))
             {
                 fileWriter.WriteLine("WARNING! Separation to small between " + plane1._tag + " and " + plane2._tag + " at " + plane2._time);
             }
